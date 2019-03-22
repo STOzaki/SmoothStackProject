6d138a441c263dc4b6302f345201b4cc37f290d3
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-22 15:05:05 -0400

    modified book model for dao

--- Project1/src/com/lms/model/Book.java
+++ Project1/src/com/lms/model/Book.java
@@ -2,39 +2,39 @@ package com.lms.model;
 
 public final class Book extends POJO {
 	private String title;
-	private Author author;
-	private Publisher publisher;
+	private int authorId;
+	private int publisherId;
 
-	public Book(int id, String title, Author author, Publisher publisher) {
+	public Book(int id, String title, int authorId, int publisherId) {
 		super(id);
 		this.title = title;
-		this.author = author;
-		this.publisher = publisher;
+		this.authorId = authorId;
+		this.publisherId = publisherId;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public Author getAuthor() {
-		return author;
-	}
-
-	public Publisher getPublisher() {
-		return publisher;
-	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	public void setAuthor(Author author) {
-		this.author = author;
+	public int getAuthorId() {
+		return authorId;
 	}
 
-	public void setPublisher(Publisher publisher) {
-		this.publisher = publisher;
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
 	}
-	
+
+	public int getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(int publisherId) {
+		this.publisherId = publisherId;
+	}
+
 
 }
