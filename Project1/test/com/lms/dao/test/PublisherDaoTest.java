e7ba2e2c072c61951c50ffe738ab4cb7367fd410
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-23 17:00:57 -0400

    test for publisher to work with change

--- Project1/test/com/lms/dao/test/PublisherDaoTest.java
+++ Project1/test/com/lms/dao/test/PublisherDaoTest.java
@@ -20,22 +20,32 @@ public class PublisherDaoTest {
 
 	private Publisher publisher;
 	private PublisherDaoImpl publisherDaoIml;
-	private static int ID = 10000;
+	private int newPublisherId;
 	
 	@BeforeEach
-	void init() {
-		publisher = new Publisher(ID, "publisher name", "publisher address", "1234567890");
+	void init() throws IOException {
 		publisherDaoIml = new PublisherDaoImpl();
+		publisher = new Publisher(789, "publisher name", "publisher address", "1234567890");
+		publisherDaoIml.save(publisher);
+		
+		newPublisherId = 0;
+		if(publisherDaoIml.findAll().size() - 1 > -1) {
+			int lastAuthorIndex = publisherDaoIml.findAll().size() - 1;
+			newPublisherId = publisherDaoIml.findAll().get(lastAuthorIndex).getId();
+		}
+		
 	}
 	
 	@AfterEach
 	void tearThis() throws FileNotFoundException, IOException {
-		// WARNING maybe something that doesnt call the method we are trying to test
-		publisherDaoIml.delete(ID);
+		// WARNING maybe something that doesn't call the method we are trying to test
+		publisherDaoIml.delete(newPublisherId);
 	}
 
 	@Test
 	public void saveTest() throws FileNotFoundException, IOException {
+		publisherDaoIml.delete(newPublisherId);
+		
 		int previousCount;
 		previousCount = publisherDaoIml.findAll().size();
 		publisherDaoIml.save(publisher);
@@ -45,32 +55,31 @@ public class PublisherDaoTest {
 	
 	@Test
 	public void deleteTest() throws FileNotFoundException, IOException {
-		publisherDaoIml.save(publisher);
 		int previousCount = publisherDaoIml.findAll().size();
-		publisherDaoIml.delete(ID);
+		publisherDaoIml.delete(newPublisherId);
 		int currentCount = publisherDaoIml.findAll().size();
 		assertTrue(previousCount > currentCount);
 	}
 	
 	@Test
 	public void findPublisherTest() throws FileNotFoundException, IOException {
-		publisherDaoIml.save(publisher);
-		assertEquals(publisher.getId(), publisherDaoIml.find(ID).getId());
+		assertEquals(newPublisherId, publisherDaoIml.find(newPublisherId).getId());
 	}
 	
 	@DisplayName("return null for publisher not found")
 	@Test
 	public void notFindPublisherTest() throws IOException {
-		assertNull(publisherDaoIml.find(ID));
+		publisherDaoIml.delete(newPublisherId);
+		
+		assertNull(publisherDaoIml.find(newPublisherId));
 	}
 	
 	@DisplayName("Will update correct")
 	@Test
 	public void updatePublisherTest() throws FileNotFoundException, IOException {
-		publisherDaoIml.save(publisher);
-		Publisher newUpdate = new Publisher(ID, "publisher name2", "publisher address2", "0987654321");
+		Publisher newUpdate = new Publisher(newPublisherId, "publisher name2", "publisher address2", "0987654321");
 		publisherDaoIml.update(newUpdate);
-		Publisher newPublisher = publisherDaoIml.find(ID);
+		Publisher newPublisher = publisherDaoIml.find(newPublisherId);
 		assertEquals(newPublisher.getId(), newUpdate.getId());
 		assertTrue(newPublisher.getPublisherName().equals(newUpdate.getPublisherName()));
 		assertTrue(newPublisher.getPublisherAddress().equals(newUpdate.getPublisherAddress()));
