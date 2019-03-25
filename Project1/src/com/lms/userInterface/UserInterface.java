57f50f5f5dca6b27af243ce05d7850c52c6e7991
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-03-24 21:00:37 -0400

    added an interface

---
+++ Project1/src/com/lms/userInterface/UserInterface.java
@@ -0,0 +1,232 @@
+package com.lms.userInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.lms.model.Author;
+import com.lms.model.Book;
+import com.lms.model.Publisher;
+import com.lms.service.AuthorService;
+import com.lms.service.BookService;
+import com.lms.service.PublisherService;
+
+public class UserInterface {
+	private static String[] dataBases = {"Author", "Book", "Publisher"};
+	private static String[] authorOptions = {"Author Name"};
+	private static String[] bookOptions = {"Title", "Author Id", "Publisher Id"};
+	private static String[] publisherOptions = {"Publisher Name", "Publisher Address", "Publisher Phone Number"};
+	private static String[] operationOptions = {"Save", "Delete", "Update", "Find", "Find All"};
+
+	
+	private static int whichOperation(Scanner userInput) {
+		System.out.println("What would you like to do with the database?");
+		iterateOptions(operationOptions);
+		return userInput.nextInt();
+	}
+	
+	private static List<String> infoGathering(Scanner userInput, String[] list) {
+		List<String> returnInfo = new ArrayList<>();
+		for(int i = 0; i < list.length; i++) {
+			System.out.println("What is the " + list[i] + "?");
+			returnInfo.add(userInput.next());
+		}
+		return returnInfo;
+	}
+	
+	private static void iterateOptions(String[] list) {
+		for(int i = 0; i < list.length; i++) {
+			System.out.println(i + 1 + ". " + list[i]);
+		}
+	}
+	
+	private static int promptForId(Scanner userInput, String method) {
+		System.out.println("What is the Id of the entry you are trying to " + method + "?");
+		return userInput.nextInt();
+	}
+	
+	private static List<String> updateInfo(Scanner userInput, String[] list) {
+		List<String> returnInfo = new ArrayList<>();
+		for(int i = 0; i < list.length; i++) {
+			System.out.println("What would you like " + list[i] + " to be updated to? (hit enter to skip)");
+			returnInfo.add(userInput.next());
+		}
+		return returnInfo;
+	}
+	
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Which database would you like to use?");
+		iterateOptions(dataBases);
+
+		int dataChoice = userInput.nextInt();
+		int operation = -1;
+		List<String> info = new ArrayList<>();
+		switch (dataChoice) {
+		case 1:
+			// Author database
+			operation = whichOperation(userInput);
+			switch (operation) {
+			case 1:
+				// Author save
+				info = infoGathering(userInput, authorOptions);
+				System.out.println("This is the AuthorID associated with your entry: " + AuthorService.saveAuthor(info.get(0)));
+				break;
+			case 2:
+				// Author delete
+				Author deletedEntry = AuthorService.deleteAuthor(promptForId(userInput, "delete"));
+				if(deletedEntry == null) {
+					System.out.println("Unfortunatly, we failed to delete that entry");
+				} else {
+					System.out.println("Successfully deleted entry: (Info about deleted entry)");
+					System.out.println(deletedEntry.toString());
+				}
+				break;
+			case 3:
+				// Author update
+				System.out.println("What is the Author Id that you would like to update?");
+				int authorId = userInput.nextInt();
+				List<String> updateList = updateInfo(userInput, authorOptions);
+				Author updatedAuthor = new Author(authorId, updateList.get(0));
+				AuthorService.updateAuthor(updatedAuthor);
+				break;
+			case 4:
+				// Author find
+				Author foundAuthor = AuthorService.findAuthor(promptForId(userInput, "find"));
+				if(foundAuthor == null) {
+					System.out.println("Could not locate that entry");
+				} else {
+					System.out.println(foundAuthor.toString());
+				}
+				break;
+			case 5:
+				// Author findAll
+				List<Author> allAuthors = AuthorService.findAllAuthors();
+				allAuthors.parallelStream().forEach(a -> System.out.println(a.toString()));
+				break;
+			default:
+				System.out.println("No such operation.");
+			}
+			break;
+		case 2:
+			// Book database
+			operation = whichOperation(userInput);
+			switch (operation) {
+			case 1:
+				// Book save
+				info = infoGathering(userInput, bookOptions);
+				int[] ids = BookService.saveBook(info.get(0), Integer.parseInt(info.get(1)), Integer.parseInt(info.get(2)));
+				System.out.println("This is the BookID associated with your entry: " + ids[0]);
+				System.out.println("BookId: " + ids[1]);
+				System.out.println("PublisherId" + ids[2]);
+				break;
+			case 2:
+				// Book delete
+				Book deletedEntry = BookService.deleteBook(promptForId(userInput, "delete"));
+				if(deletedEntry == null) {
+					System.out.println("Unfortunatly, we failed to delete that entry");
+				} else {
+					System.out.println("Successfully deleted entry: (Info about deleted entry)");
+					System.out.println(deletedEntry.toString());
+				}
+				break;
+			case 3:
+				// Book Update
+				System.out.println("What is the Book Id that you would like to update?");
+				int authorId = userInput.nextInt();
+				List<String> updateList = updateInfo(userInput, authorOptions);
+				Book updatedauthor = new Book(authorId, updateList.get(0), Integer.parseInt(updateList.get(0)),
+						Integer.parseInt(updateList.get(1)));
+				boolean[] results = BookService.updateBook(updatedauthor);
+				if(!results[0]) {
+					System.out.println("Book failed to update.");
+				}
+				
+				if(!results[1]) {
+					System.out.println("The provided Author Id does not exist.");
+				}
+				
+				if(!results[2]) {
+					System.out.println("The provided Publisher Id does not exist");
+				}
+				
+				if(results[0] && results[1] && results[2]) {
+					System.out.println("Book updated successfully");
+				}
+				break;
+			case 4:
+				// Book find
+				Book foundBook = BookService.findBook(promptForId(userInput, "find"));
+				if(foundBook == null) {
+					System.out.println("Could not locate that entry");
+				} else {
+					System.out.println(foundBook.toString());
+				}
+				break;
+			case 5:
+				// Book findAll
+				List<Book> allBooks = BookService.findAllBooks();
+				allBooks.parallelStream().forEach(a -> System.out.println(a.toString()));
+				break;
+			default:
+				System.out.println("No such operation.");
+			}
+			break;
+		case 3:
+			// Publisher database
+			operation = whichOperation(userInput);
+			switch (operation) {
+			case 1:
+				// Publisher save
+				info = infoGathering(userInput, publisherOptions);
+				System.out.println("This is the PublisherID associated with your entry: " + 
+						PublisherService.savePublisher(info.get(0), info.get(1), info.get(2)));
+				break;
+			case 2:
+				// Publisher delete
+				Publisher deletedEntry = PublisherService.deletePublisher(promptForId(userInput, "delete"));
+				if(deletedEntry == null) {
+					System.out.println("Unfortunatly, we failed to delete that entry");
+				} else {
+					System.out.println("Successfully deleted entry: (Info about deleted entry)");
+					System.out.println(deletedEntry.toString());
+				}
+				break;
+			case 3:
+				// Publisher Update
+				System.out.println("What is the Publisher Id that you would like to update?");
+				int publisherId = userInput.nextInt();
+				List<String> updateList = updateInfo(userInput, publisherOptions);
+				Publisher updatedPublisher = new Publisher(publisherId, updateList.get(0), updateList.get(1), updateList.get(2));
+				PublisherService.updatePublisher(updatedPublisher);
+				break;
+			case 4:
+				// Publisher find
+				Publisher publisher = PublisherService.findPublisher(promptForId(userInput, "find"));
+				if(publisher == null) {
+					System.out.println("Could not locate that entry");
+				} else {
+					System.out.println(publisher.toString());
+				}
+				break;
+			case 5:
+				// Publisher findAll
+				List<Book> allBooks = BookService.findAllBooks();
+				allBooks.parallelStream().forEach(a -> System.out.println(a.toString()));
+				break;
+			default:
+				System.out.println("No such operation.");
+			}
+			break;
+		default:
+			// No database
+			System.out.println("I am sorry that is not a database.");
+			break;
+		}
+		
+		
+		userInput.close();
+			
+	}
+
+}
